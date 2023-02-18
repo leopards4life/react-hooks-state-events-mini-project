9a149628c41fe335149e5a2a4fa0a012b89c1f19
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-02-18 13:33:11 -0700

    Add tasks to list

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Task() {
+function Task({ key, text, onDeleteTask }) {
+  function  handleClick() {
+    onDeleteTask(text);
+  }
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{key}</div>
+      <div className="text">{text}</div>
+      <button 
+      className="delete" 
+      onClick={handleClick}>X</button>
     </div>
   );
 }
